43328f5fb5bf62a56105c600cdf6537f9fb2faa2
unknown <shreyesh2008@gmail.com>
2019-12-18 13:01:58 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,15 +6,21 @@ const express=require("express");
 const app=express();
 const expressLayouts=require("express-ejs-layouts");
 const indexRouter=require("./routes/index");
+const authorRouter=require("./routes/authors")
 const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
 
 app.set("view engine","ejs");
 app.set("views",__dirname+"/views");
 app.set("layout","layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({limit:"10mb",extended:false}));
+
+app.use("/authors",authorRouter);
 app.use("/",indexRouter);
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
+
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology:true});
 const db=mongoose.connection;
 db.on("error",(error)=>console.log(error));
 db.once("open",()=>console.log("Connected to database"));
