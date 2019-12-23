a9ec5c7e11244c0a03e004e3ae556f893a3041ef
unknown <shreyesh2008@gmail.com>
2019-12-23 15:26:58 +0530

    Authors complete

--- server.js
+++ server.js
@@ -10,17 +10,21 @@ const authorRouter=require("./routes/authors")
 const bookRouter=require("./routes/books");
 const mongoose=require("mongoose");
 const bodyParser=require("body-parser");
+const methodOverride=require("method-override")
 
 app.set("view engine","ejs");
 app.set("views",__dirname+"/views");
 app.set("layout","layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"))
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({limit:"10mb",extended:false}));
+
 
 app.use("/books",bookRouter);
 app.use("/authors",authorRouter);
 app.use("/",indexRouter);
+
 
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology:true});
 const db=mongoose.connection;
